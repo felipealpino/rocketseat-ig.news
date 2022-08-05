245fb55158200e3a2646176ec0588eb4b00aa097
felipealpino <felipealpino1@gmail.com>
2022-08-04 21:00:06 -0300

    Salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query } from 'faunadb';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,7 +17,31 @@ export default NextAuth({
     }),
     // ... add more providers here
   ],
-
+  jwt: { secret: process.env.JWT_SECRET },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log('🚀 ~ user', user);
+      try {
+        await fauna.query(
+          query.Create(query.Collection('users'), {
+            data: { email: user.email },
+          })
+        );
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+    async session({ session, user, token }) {
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      return token;
+    },
+  },
   //Adatabase is optional,but required to persist accounts inadatabase
   // database: process.env.DATABASE_URL,
 });
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,6 +1,6 @@
 import { Client } from 'faunadb';
 
-
 export const fauna = new Client({
   secret: process.env.FAUNADB_KEY,
+  domain: 'db.us.fauna.com', // adicionado por causa do error de permissão 
 });
